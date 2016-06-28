f7ed6b22933f112a17b4bd51748e0ffa1d36d796
James Hildreth <hildreth.james@gmail.com>
2016-06-27 20:25:53 -0400

    final rework

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+$(document).ready(function(){
+    document.querySelector('.pluginDemonstration0').setAttribute('style','display:block');
+    document.querySelector('.pluginDemonstration1').setAttribute('style','display:none');
+    document.querySelector('.pluginDemonstration2').setAttribute('style','display:none');
+    document.querySelector('.pluginDemonstration3').setAttribute('style','display:none');
+
+    document.querySelector('.plugin1').addEventListener('click', function(){
+        document.querySelector('.pluginDemonstration0').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration1').setAttribute('style','display:block');
+        document.querySelector('.pluginDemonstration2').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration3').setAttribute('style','display:none');
+    });
+
+    document.querySelector('.plugin2').addEventListener('click', function(){
+        document.querySelector('.pluginDemonstration0').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration1').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration2').setAttribute('style','display:block');
+        document.querySelector('.pluginDemonstration3').setAttribute('style','display:none');
+    });
+
+    document.querySelector('.plugin3').addEventListener('click', function(){
+        document.querySelector('.pluginDemonstration0').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration1').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration2').setAttribute('style','display:none');
+        document.querySelector('.pluginDemonstration3').setAttribute('style','display:block');
+    });
+
+
+    $('.lettering-demo').lettering();
+    for(var i = 0; i < 1000; i++) {
+        var className = ".char" + (i+1).toString();
+        var elem = $(className);
+        if(elem != null) {
+            var color = Math.floor(Math.random()*16777215).toString(16);
+            elem.css({"color": "#"+color});
+        }
+    }
+})
+
+
 
 $(":submit").click(function () {
 $("#showme").show();
@@ -6,7 +46,7 @@ $("#showme").show();
        speed: 200,
        lifeLike: true,
        startDelay: 3000,
-       deleteSpeed: 100
+       deleteSpeed: 100,
   })
   .tiPause(200)
   .tiType('I really like Front End Web Development. ')
